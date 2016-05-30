77e5c7089e15725684b105f1d98fa1c3633f2517
rushil-patel <rushilpatel10@gmail.com>
2016-05-29 20:01:41 -0700

    pokemon and types

---
+++ PokeDuelServer/src/Pokemon.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author rushi_000
+ */
+public class Pokemon {
+    
+    public int id;
+    public String name;
+    public int cost;
+    public int[] stats;
+    public String sprite;
+    public int[] resistances;
+    public int[] types;
+    public int numTypes;
+    
+    public Pokemon(int id, String name, int cost, int[] stats, String sprite,
+            int[] resistances, int[] types, int numTypes)
+    {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.stats = stats;
+        this.sprite = sprite;
+        this.resistances = resistances;
+        this.types = types;
+        this.numTypes = numTypes;
+    }
+    
+}
---
+++ PokeDuelServer/src/Types.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author rushi_000
+ */
+public enum Types 
+{
+    NORMAL(0), FIGHT(1), FLYING(2), POISON(3), GROUND(4),
+    ROCK(5), BUG(6), GHOST(7), STEEL(8), FIRE(9), WATER(10),
+    GRASS(11), ELECTR(12),
+    PSYCHC(13), ICE(14), DRAGON(15), DARK(16);
+    
+    private final int value;
+    
+    private Types(int value)
+    {
+        this.value = value;
+    }
+    public int getValue()
+    {
+        return value;
+    }
+}
